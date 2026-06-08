4b75ea6fe8652abe6ead57f2d129bd2dbd664f7c
Kussi27101989 <Kussi89@gmx.de>
2026-06-08 20:57:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,33 @@
-const CACHE_NAME = 'fitplan-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_VERSION = 'fitplan-v' + Date.now();
+const CACHE_NAME = CACHE_VERSION;
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(['./index.html', './manifest.json']))
+  );
+  // Sofort aktivieren – nicht auf Tab-Schließen warten
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  // Alle offenen Tabs sofort übernehmen
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  // Network first – immer frische Version von GitHub holen
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
